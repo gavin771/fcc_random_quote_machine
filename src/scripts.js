bf71421d9a7fc0a418420661b0185b17e26e89d4
gavin771 <gavin.samuels10@gmail.com>
2017-08-02 05:26:09 -0500

    updated script to use a new api

--- src/scripts.js
+++ src/scripts.js
@@ -3,15 +3,17 @@ $(document).ready(function () {
     $("#new-message").on("click", function () {
 
         $.ajax({
-            url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
-            cache: false
+            url: "https://api.forismatic.com/api/1.0/?",
+            dataType: "jsonp",
+            data: "method=getQuote&format=jsonp&lang=en&jsonp=?"
         }).done(function (data) {
 
             var html = "";
             var currentTime = moment().format("h:mm:ss A");
 
             $("#last-gnerated").html(currentTime);
-            console.log(data[0].content);
+            $("#message").html(data.quoteText);
+            $("#author").html("- "+ data.quoteAuthor);
         }).fail(function () {
             $("#message").html("A new quote couldn't be retrieved... :(");
         });
