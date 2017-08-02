33e3cfbe3b34e6e3392262f1bf21fb102a77762c
gavin771 <gavin.samuels10@gmail.com>
2017-08-02 03:57:04 -0500

    modified build and deploy steps

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
-gulp.task('deploy', ['build'], () => {
-  return gulp.src('dist')
-    .pipe($.subtree())
-    .pipe($.clean());
+var gulp = require('gulp');
+var deploy = require('gulp-gh-pages');
+var install = require("gulp-install");
+
+gulp.task('build', function () {
+  gulp.src(['./bower.json', './package.json'])
+    .pipe(gulp.dest('./dist/'))
+    .pipe(install());
+});
+
+gulp.task('deploy', ['build'], function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
 });
