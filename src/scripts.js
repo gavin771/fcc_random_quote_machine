dcfc2bf55c8a3498f7324aff38ecd351743432b3
gavin771 <gavin.samuels10@gmail.com>
2017-08-02 05:38:49 -0500

    Update 2017-08-02T10:38:22.457Z

--- src/scripts.js
+++ src/scripts.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
 
     $("#new-message").on("click", function () {
 
+        loadQuote()
+    });
+
+    function loadQuote() {
         $.ajax({
             url: "https://api.forismatic.com/api/1.0/?",
             dataType: "jsonp",
@@ -13,9 +17,13 @@ $(document).ready(function () {
 
             $("#last-gnerated").html(currentTime);
             $("#message").html(data.quoteText);
-            $("#author").html("- "+ data.quoteAuthor);
+            $("#author").html("- " + data.quoteAuthor);
+            $("#tweet").attr("href", "https://twitter.com/home/?status=" + data.quoteText +
+                ' (' + data.quoteAuthor + ')');
         }).fail(function () {
             $("#message").html("A new quote couldn't be retrieved... :(");
         });
-    });
+    }
+
+    loadQuote();
 });
