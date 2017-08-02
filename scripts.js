65237e47b9db24fbf367cb242e4f0d4161a56a69
gavin771 <gavin.samuels10@gmail.com>
2017-08-02 03:21:08 -0500

    modified ajax to retrieve quote

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
 $(document).ready(function () {
 
     $("#new-message").on("click", function () {
-        $.getJSON("/json/call", function (json) {
+
+        $.ajax({
+            url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+            cache: false
+        }).done(function (data) {
 
             var html = "";
             var currentTime = moment().format("h:mm:ss A");
 
             $("#last-gnerated").html(currentTime);
-            console.log(json);
-
+            console.log(data[0].content);
         }).fail(function () {
             $("#message").html("A new quote couldn't be retrieved... :(");
         });
