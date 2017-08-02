5b3199d1f9f1d44ba9a3d373dcc6d441938ebdb4
gavin771 <gavin.samuels10@gmail.com>
2017-08-02 04:20:14 -0500

    Update 2017-08-02T09:18:21.352Z

---
+++ src/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    $("#new-message").on("click", function () {
+
+        $.ajax({
+            url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+            cache: false
+        }).done(function (data) {
+
+            var html = "";
+            var currentTime = moment().format("h:mm:ss A");
+
+            $("#last-gnerated").html(currentTime);
+            console.log(data[0].content);
+        }).fail(function () {
+            $("#message").html("A new quote couldn't be retrieved... :(");
+        });
+    });
+});
